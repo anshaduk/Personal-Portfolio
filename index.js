68f2efb13004e011ddf95fd0563a1b3b162981b4
Anshad UK <anshadu@gmail.com>
2024-11-15 20:17:42 +0530

    feat: "update portfolio"

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-function sendMail() {
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-  if((document.getElementById("inputName").value.length>0)&&(document.getElementById("inputEmail").value.length>0)&&(document.getElementById("inputEmail").value.includes("@"))&&(document.getElementById("inputMessage").value.length>0)){
-    var params = {
-      name: document.getElementById("inputName").value,
-      email: document.getElementById("inputEmail").value,
-      message: document.getElementById("inputMessage").value
-    };
-    const serviceID = "service_amih1xh";
-    const templateID = "template_aukveh8";
-  
- 
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-      document.getElementById("inputName").value = "";
-      document.getElementById("inputEmail").value = "";
-      document.getElementById("inputMessage").value = "";
-      console.log(res);
-      alert("your message send successfully..!"); 
-      
-    })
-    .catch((err) => console.log(err));
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  alert(`The email was successfully sent.`);
+});
+
+function sendMail(){
+  var params={
+      name:document.getElementById("name").value,
+      email:document.getElementById("email").value,
+      message:document.getElementById("message").value
+  };
+
+const serviceID="service_2qw8wsh";
+const templateID="template_uqgdcjs";
+
+
+ emailjs
+ .send(serviceID,templateID,params)
+ .then((res)=>{
+      document.getElementById("name").value="";
+      document.getElementById("email").value="";
+      document.getElementById("message").value="";
   }
-  else{
-    alert("Please fill the field...")
-  }
-  }
+ )
+ .catch((err)=> console.log(err));
+}
